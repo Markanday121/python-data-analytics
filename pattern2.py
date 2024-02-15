c0430d7c209fee01f4024789c3bceb72a21a6e78
Markanday121 <markanday.gupta121@gmail.com>
2024-02-15 20:18:55 +0530

    practice

---
+++ pattern2.py
@@ -0,0 +1,15 @@
+from turtle import*
+speed(0)
+
+def polygon(side):
+    for _ in range(side):
+        fd(100)
+        lt(360/side)
+# tesing the function
+for i in range(6):
+    polygon(10)
+    polygon(6)
+    polygon(4)
+    lt(60)
+hideturtle()
+mainloop()
